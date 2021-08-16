593d320f9e40407aedc98450880d1737ae3583c1
MOHAMEDAfrath <afrathismath@gmail.com>
2021-08-16 14:31:07 +0530

    [Afrath]updated the file with modifying existing contacts

--- Contacts.js
+++ Contacts.js
@@ -5,16 +5,16 @@ var addressbook = new Array();
 var option = prompt('Enter 1 to create address book : ');
 switch(option){
     case "1":
-        //UC-2 created addressbook array
+        //UC-3 created addressbook array
         var addressbook = new Array();
         console.log("Address Book created!");
         while(true){
-            console.log("Enter 1 to add details to book \nEnter 2 to Display \nEnter 3 to Exit\n");
+            console.log("\nEnter 1 to add details to book \nEnter 2 to Display \nEnter 3 to Modify Exisiting Contact \nEnter 4 to Exit\n");
             var options = prompt("Enter option: ");
         switch(options){
             case "1":
                 console.log("****Add Option****");
-                //UC-2 add contacts to address book array
+                //UC-3 add contacts to address book array
                 AddContacts(addressbook);
                 break;
             case "2":
@@ -22,6 +22,10 @@ switch(option){
                 Display(addressbook);
                 break;
             case "3":
+                console.log("***Modify contacts***");
+                Modify(addressbook);
+                break;
+            case "4":
                 return;
         }
     }
@@ -46,17 +50,72 @@ function AddContacts(address) {
     contact.phone = prompt("Enter phone number : ");
     contact.email = prompt("Enter email : ");
     address.push(contact);
+    console.log(`Added ${contact.firstName} successfully`);
   } catch (e) {
     console.log(e);
   }
   return address;
 }
+//UC-4 Modify existing details
+function Modify(address)
+{   
+    var firstName = prompt('Enter first name :');
+    var result = address.find(x=>x.firstName == firstName);
+    if(result == null){
+        console.log("Enter valid name!")
+    }else{
+        console.log("Enter 1 to modify firstname \nEnter 2 to modify lastname \nEnter 3 to modify address \nEnter 4 to modify city name");
+        console.log("Enter 5 to modify state name \nEnter 6 to modify zip \nEnter 7 to modify phone number \nEnter 8 to modify email");
+        try{
+            var option = prompt('Enter option: ');
+            switch(option){
+                case "1":
+                    result.firstName = prompt('Enter new First Name: ');
+                    console.log("First Name updated");
+                    break;
+                case "2":
+                    result.lastName = prompt('Enter new last Name: ');
+                    console.log("last Name updated");
+                    break;
+                case "3":
+                    result.address = prompt('Enter new address: ');
+                    console.log("address updated");
+                    break;
+                case "4":
+                    result.City = prompt('Enter new City Name: ');
+                    console.log("City Name updated");
+                    break;
+                case "5":
+                    result.State = prompt('Enter new State Name: ');
+                    console.log("State Name updated");
+                    break;
+                case "6":
+                    result.zip = prompt('Enter new zip: ');
+                    console.log("Zip updated");
+                    break;
+                case "7":
+                    result.phone = prompt('Enter new Phone Number: ');
+                    console.log("Phone number updated");
+                    break;
+                case "8":
+                    result.email = prompt('Enter new Email ID: ');
+                    console.log("Email Id updated");
+                    break;
+                default:
+                    break;
 
+            }
+        }catch(e){
+            console.log(e);
+        }
+    }
+}
+//displays the contact
 function Display(address){
     if(address.length>0){
     for(let i = 0;i<address.length;i++){
         console.log(`Contact ${i+1} :-`)
-        console.log(address.toString()+"\n");
+        console.log(address[i].toString()+"\n");
     }
 }else{
     console.log("No contacts found!");
