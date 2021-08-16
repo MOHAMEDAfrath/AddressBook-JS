d90b9b6cc6f6dd896a5740d9a6ba8046f7217dbd
MOHAMEDAfrath <afrathismath@gmail.com>
2021-08-16 12:05:02 +0530

    [Afrath]Updated the file with create address book with name,address,contact details

--- Contacts.js
+++ Contacts.js
@@ -1,13 +1,15 @@
 //getting the addressbook from another js file
 const {AddressBook} = require('./AddressBook');
+var prompt = require('prompt-sync')();
 //creating object for the class
 let contact = new AddressBook();
-contact.firstName = "Mohamed";
-contact.lastName = "Afrath";
-contact.address = "Menod Street";
-contact.City = "Chennai";
-contact.State = "Tamil Nadu";
-contact.phone = 9841635374;
-contact.email = "afrath@gmail.com";
+contact.firstName = prompt('Enter first name : ');
+contact.lastName = prompt('Enter last name : ');
+contact.address = prompt('Enter address : ');
+contact.City = prompt('Enter city name : ');
+contact.State = prompt('Enter state name : ');
+contact.zip = prompt('Enter ZipCode: ')
+contact.phone = prompt('Enter phone number : ');
+contact.email = prompt('Enter email : ');
 //printing with toString method
 console.log(contact.toString());
